83cc3a3b3a9a22716cc9141876f83fb88df82357
BiancaBarrosGithub <biancapl.barros@gmail.com>
2024-03-26 16:51:45 -0300

    Links

---
+++ src/css/indexStyle.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+ export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    btn: {
+        backgroundColor:"red",
+        color: "white",
+        padding: 25,
+        margin:30,
+        borderRadius: 5
+
+    },
+
+    imagens: {
+        height:200,
+        width: 200,
+      },
+  });
